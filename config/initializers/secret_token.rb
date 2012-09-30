fd4ab28f11dcf24fb6eb2e9eab5d5bed2001978d
daifu <daifu.ye@gmail.com>
2012-09-29 21:24:08 -0700

    Created application using Refinery CMS 2.0.8

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HccuclaRails::Application.config.secret_token = '388850721d4fce351d2d82a6eda928ad29b44462031ad1d1ad452a4bf810e28e8815c7f2f79e13d2076838f3e6d646fa83e45369f2a4a0bb01b84908412fd340'
